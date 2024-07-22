5f87e4fe07fd317c06007f26fb0a0af241cab5dc
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-07-22 19:26:00 +0000

    proyecto finalizado

--- src/app.py
+++ src/app.py
@@ -25,6 +25,7 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# generate getAllmembers endpoints
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
@@ -37,6 +38,52 @@ def handle_hello():
 
 
     return jsonify(response_body), 200
+
+# generate addMember endPoint
+@app.route('/member', methods=['POST'])
+def add_member():
+    
+    # Verifico si el Content-Type de la solicitud es del tipo application/json,sino devuelvo un error 400
+    if request.content_type != 'application/json':
+        return jsonify({"error": "Content-Type must be application/json"}), 400
+    #Convierto los datos que provienen del http en formato json y luego a diccionario de Phyton. 
+    member_data = request.get_json()
+    
+    #Realizo las validaciones correspondientes a las propiedades del miembro,sino lanzo un error 400
+    if not isinstance(member_data["age"], int):
+        return jsonify({"error": "'age' debe contener valor/o ser un entero"}), 400
+    if not isinstance(member_data["first_name"], str):
+        return jsonify({"error": "'first_name' debe ser una cadena/no puede quedar vacia"}), 400
+    if not isinstance(member_data["lucky_numbers"], list):
+        return jsonify({"error": "'lucky_numbers' must be a list"}), 400
+    if not all(isinstance(num, int) for num in member_data["lucky_numbers"]):
+        return jsonify({"error": "All 'lucky_numbers' must be integers"}), 400
+    
+    #Generamos un espacio de memoria que va a almacenar un nuevo miembro de la familia Jackson,
+    # haciendo uso de la instancia "jackson_family y su metodo add.member.Por ultimo recibimos un miembro como respuesta"
+    new_member = jackson_family.add_member(member_data)
+    #Devolvemos una respuesta HTTP en formato JSON y su codigo de estado "200", que fue exitosa la operacion.
+    return jsonify(new_member), 200
+
+# generate oneGetMember endPoint
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+
+    capture_member= jackson_family.get_member(id)  
+    if capture_member:
+        return jsonify(capture_member),200
+    else:
+        return jsonify({"error":"No se encontro el miembro"}), 404
+
+# generate deleteMember endPoint
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+
+    capture_delete_member= jackson_family.delete_member(id)  
+    if capture_delete_member:
+        return jsonify({"mensaje":"miembro eliminado", "miembo": capture_delete_member}),200
+    else:
+        return jsonify({"error":"No se puede eliminar,porque nose encontro al miembro"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
